ee3c3e2358f362346aa32e7d4aeeb345f2553327
Gary Little <gary.j.little@gmail.com>
2023-10-26 18:40:12 -0400

    Added bandwidth

--- ntnx_vmtop.py
+++ ntnx_vmtop.py
@@ -39,7 +39,7 @@ def main():
         os.system('clear')
         #Print header and API response time
         print(f"API response time {api_resp:3.2f}s with {str(num_results):4} entries")
-        print(f'{"vmname":>30} {"GB":>5} {"VCPU":>5} {"IOPS":>7} {"CPU%":>5} {"State":>5}')
+        print(f'{"vmname":>30} {"GB":>5} {"VCPU":>5} {"IOPS":>7} {"CPU%":>5} {"State":>5} {"BW MB/s":>9} {"BW-Read MB/s":>9} {"BW-Write MB/s":>9}')
         
         vmlist=sorted(res, key=lambda f: int(f['stats']["hypervisor_cpu_usage_ppm"]),reverse=True)
         count=1
@@ -47,13 +47,19 @@ def main():
             vm=vmlist[i]
             # skip CVM and unhosted/powered off VMs
             if not vm["controllerVm"] and (vm["powerState"] == "on"):
+                vm_total_bw_kbps=vm["stats"]["controller_io_bandwidth_kBps"]
+                vm_total_bw_mbps=int(int(vm_total_bw_kbps)/1000)
+                vm_total_read_bw_kbps=vm["stats"]["controller_read_io_bandwidth_kBps"]
+                vm_total_read_bw_mbps=int(int(vm_total_read_bw_kbps)/1000)
+                vm_total_write_bw_kbps=vm["stats"]["controller_write_io_bandwidth_kBps"]
+                vm_total_write_bw_mbps=int(int(vm_total_write_bw_kbps)/1000)
                 vm_powerstate=vm["powerState"] 
                 vm_name=vm["vmName"]
                 vm_vcpu=vm["numVCpus"]
                 vm_mem_gb=int((int(vm["memoryCapacityInBytes"])/1024**3))
                 vm_iops=vm["stats"]["controller_num_iops"]
                 vm_cpu_pct=((int(vm["stats"]["hypervisor_cpu_usage_ppm"])/1000000)*100)
-                print(f'{vm_name:>30.30} {vm_mem_gb:>5} {vm_vcpu:>5} {vm_iops:>7} {vm_cpu_pct:>5.2f} {vm_powerstate:>5} ')
+                print(f'{vm_name:>30.30} {vm_mem_gb:>5} {vm_vcpu:>5} {vm_iops:>7} {vm_cpu_pct:>5.2f} {vm_powerstate:>5} {vm_total_bw_mbps:>9} {vm_total_read_bw_mbps:>9} {vm_total_write_bw_mbps:>9}')
                 count=count+1
             if (count>vms_to_list):
                 break    
